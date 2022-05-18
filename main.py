222b1bb3067c51b894ada26ba7da531a5cc2f7e0
hryvnia <105789022+Sanagocat@users.noreply.github.com>
2022-05-18 11:49:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+#count = 0 ~ until count < 10 > count +1
+for count in range (10): 
+  print("Do Repeat : "+ str(count))
+
+#start from test = 0 
+for test in range (20):
+  print("hello!" + str(test))
+
+# k , 1000 , "Time is ticking.. 0~999"
+for k in range (1000):
+  print("Time is ticking " + str(k))  
+
+# f=0 ~ f <100 f+1
+for f in range (100):
+  print("HAHAHAHAHAHAHA")
+
+# i=1  ~ i<100   i+2
+for i in range (1,100,2):
+  print(i)
+
+# from i = 10    i< 3000  10, 20 30 40 50 60 ..... 2990
+for i in range (10,2999,10):
+  print(i)
